ffaa0533ba406cf8006aebfb4d8f555a131fda1c
mike dunk <mike_4596@hotmail.com>
2019-02-06 20:21:53 -0800

    official start, start on footer

---
+++ themes/sewintune/footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package startertheme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+		<div class="store-info"
+			<div class="footer-logo">
+			</div>
+			</div>
+		<div class="contact">Contact Us
+		<div class="our-location">
+		<i class="fas fa-map-marker-alt"></i>
+		2916 50th Ave Lloydminster, Alberta, T9V 2S5
+		</div>
+		<div class="phone">
+		<i class="fas fa-phone"></i>
+		1 (780) 875-1305
+		</div>
+		<div class="email">
+		<i class="fas fa-envelope"></i>
+		<span>music@sewintune.ca
+		sewing@sewintune.ca<span>		
+		</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
