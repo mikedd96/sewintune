42c8001af45b3ac951b2db17252a3bd3d9c57cca
mike dunk <mike_4596@hotmail.com>
2019-02-07 00:08:29 -0800

    design footer

--- themes/sewintune/footer.php
+++ themes/sewintune/footer.php
@@ -15,25 +15,53 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-		<div class="store-info"
+		<div class="store-info">
 			<div class="footer-logo">
 			</div>
+			<div class="blurb">
+			<p> Mark 14:51
+A young man was following Him, wearing nothing but a <br>
+linen sheet over his naked body; and they seized him.<br>
+But he pulled free of the linen sheet and escaped naked. <br>
+Mark 14:51
+A young man was following Him, wearing nothing but a <br>
+linen sheet over his naked body; and they seized him.<br>
+But he pulled free of the linen sheet and escaped naked. <br>
+  Mark 14:51
+A young man was following Him, wearing nothing but a <br>
+linen sheet over his naked body; and they seized him.<br>
+But he pulled free of the linen sheet and escaped naked. <br>
+Mark 14:51
+A young man was following Him, wearing nothing but a <br>
+linen sheet over his naked body; and they seized him.<br>
+But he pulled free of the linen sheet and escaped naked. <br>
 			</div>
-		<div class="contact">Contact Us
+			</div>
+		<div class="contact"><span class="contact-us"> Contact Us </span>
+		<div class="contact-info">
 		<div class="our-location">
 		<i class="fas fa-map-marker-alt"></i>
-		2916 50th Ave Lloydminster, Alberta, T9V 2S5
+		<div class="address">2916 50th Ave Lloydminster, Alberta, T9V 2S5
+		</div>
 		</div>
 		<div class="phone">
 		<i class="fas fa-phone"></i>
-		1 (780) 875-1305
+		<div class="phonenumber">1 (780) 875-1305
+		</div>
 		</div>
 		<div class="email">
 		<i class="fas fa-envelope"></i>
-		<span>music@sewintune.ca
-		sewing@sewintune.ca<span>		
+		<div class="emails">Music@sewintune.ca <br>
+		Sewing@sewintune.ca</div>		
+		</div>
+		</div>
 		</div>
 		</div><!-- .site-info -->
+		<div class="social">
+		<a class="fab fa-facebook-square" href="https://facebook.com/sewintunelloydminster"> Facebook</a>
+		<a class="fab fa-instagram" href="https://instagram.com/sewintune"> Instagram</a>
+		<a class="fab fa-twitter-square" href="https://twitter.com/sewintune"> Twitter</a>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
